5532bf47e623958613a36f0eae63e3d4d6bcc7f0
Jacob-Liddicoat-CS <j.liddicoat@share.epsb.ca>
2019-01-11 11:49:31 -0700

    Starting on While Loop

--- AlphabeticalSearch/AlphabeticalSearch.java
+++ AlphabeticalSearch/AlphabeticalSearch.java
@@ -1,5 +1,6 @@
 public class AlphabeticalSearch {
   public static void main(String[] args) {
+    boolean;
     //Input String and Verify
     //String word[] = new String[9]
     String[] word = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."};
@@ -11,5 +12,19 @@ public class AlphabeticalSearch {
     word[0] = word[0].toLowerCase();
     System.out.println(word[1].compareTo(word[0]));
     //Duplicate words? Use an IF to omit the comparison
+    //Functions to use:
+        System.out.println(word[0] + " " + word[6]);
+        System.out.println(word[0] == word[6]); //correct order, returns false since comparing memory location
+        System.out.println(word[6].equals(word[0])); //correct order, returns true since comparing characters
+        //While Loop Begins
+    while (stop == false) {
+      for(int i = 0; i++) {
+        wordtoLowerCase[i].length-1
+        word[i+1].compareTo(word[i])
+
+      }
+      word[i+1].equals(word[i])
+      stop == true
+    }
   }
 }
